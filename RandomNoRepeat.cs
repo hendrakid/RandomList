b21cb834663a6dfe1299d5c1fa4ad38eeea1e1d3
kholilurrohman hendrata <hendrakid@gmail.com>
2017-01-07 15:10:20 +0700

    Add files via upload

---
+++ RandomNoRepeat.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomNoRepeat : MonoBehaviour {
+	
+	private List<string> alpha = new List<string>{"A", "B", "C", "D", "E", "F", "G", "H"};
+	private List<string> randomList = new List<string>();//move the alpa to this list
+
+	public string x;
+	public Text text;
+
+	void Start () {
+		RandomTest ();
+	}
+
+	void Update () {
+		
+	}
+	public void RandomTest(){
+
+		int countList = alpha.Count;
+		for (int i = 0; i < countList; i++) {
+			int rand = Random.Range(0,alpha.Count-1);
+			randomList.Add(alpha[rand]);//add the randomList with the alpha random index
+			alpha.RemoveAt(rand);//remove the current alpha to prevent repeats index
+			x = x +" - "+randomList[i];
+			text.text = x;//make new Text in hierarchy to check the list
+		}
+	}
+}
